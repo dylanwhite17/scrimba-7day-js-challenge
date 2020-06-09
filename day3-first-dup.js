b1883b87eb67931aa71b0e9528ece360774f357a
dylanwhite17 <46232286+dylanwhite17@users.noreply.github.com>
2020-06-09 18:57:41 -0300

    Add files via upload
    formatting
    refactoring: arrow functions and ternaries
    added a helper function to fix the duplicate in an array, provide the fixed array and provide feedback

--- day3-first-dup.js
+++ day3-first-dup.js
@@ -1,20 +1,28 @@
-const arrayWithDup = [8, 2, 3, 1, 2, 5, 3]
-const arrayNoDup =   [8, 1, 2, 5, 3, 4]
+//// FIND THE FIRST  DUPLICATE NUMBER IN AN ARRAY:
+const arrayWithDup = [8,4,2,3,1,4,2,5,3]
+const arrayNoDup =   [8,1,2,5,3,4]
 
-function firstDuplicate(nums) {
+
+const firstDuplicate = nums => {
     let newNums = []
-    for (let i=0; i< nums.length; i++) {
-        if (newNums.includes(nums[i])){
-            //returns first duplicate number in nums
-            return nums[i]
-        } else if (!newNums.includes(nums[i])) {
-            newNums.push(nums[i])
-        }  
+    let result = []
+    for (let i = 0; i < nums.length; i++) {
+        newNums.includes(nums[i]) ? result.push(nums[i]) : newNums.push(nums[i]) 
     }
     //returns -1 if no duplicates numbers in nums
-    return -1
-}
+    return result[0] || -1
+    }
 
 console.log(`${firstDuplicate(arrayWithDup)}, ${firstDuplicate(arrayNoDup)}`) //2, -1
 
+////OPTIONAL MESSING AROUND: FIX THE DUPLICATION AND PROVIDE FEEDBACK
+const fixDuplicate = (value, array) => {
+    value == -1 ? console.log(`The array you have provided contains no duplicates.`) 
 
+                : console.log(`${value} is the duplicate number in this array.`)
+                  let fixArrayToSet = new Set(array)
+                  let fixedArray = [...fixArrayToSet]
+                  return fixedArray
+}
+fixDuplicate(firstDuplicate(arrayWithDup), arrayWithDup)
+fixDuplicate(firstDuplicate(arrayNoDup), arrayNoDup)
