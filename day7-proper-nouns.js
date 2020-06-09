6cd113d508d65425bd92877c6f3b35157adf5ccd
dylanwhite17 <46232286+dylanwhite17@users.noreply.github.com>
2020-06-09 19:26:17 -0300

    Add files via upload
    minor formatting

--- day7-proper-nouns.js
+++ day7-proper-nouns.js
@@ -1,7 +1,7 @@
 const string = 'pARiS'
 const string2 = 'John'
 
-const properNounCorrection = (str) => {
+const properNounCorrection = str => {
     const firstLetter = str.slice(0,1).toUpperCase()
     const theRest = str.slice((1),(str.length)).toLowerCase()
     const properNoun = firstLetter.concat('', theRest)
